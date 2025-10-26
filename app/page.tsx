c66b1cd459117161f3f249f40fdd2007fef2844e
Ariel Alvarez <arielbalvarez@gmail.com>
2025-10-26 17:07:04 +0800

    minor update.

--- app/page.tsx
+++ app/page.tsx
@@ -5,12 +5,10 @@ import ChartControls from "@/components/chart/ChartControls";
 export default function Home() {
   return (
     <main className="min-h-screen bg-background flex flex-col">
-      {/* Chart Controls - Fixed at top */}
       <div className="flex-shrink-0">
         <ChartControls />
       </div>
       
-      {/* Chart - Takes remaining space */}
       <div className="flex-1 min-h-0">
         <Chart />
       </div>
