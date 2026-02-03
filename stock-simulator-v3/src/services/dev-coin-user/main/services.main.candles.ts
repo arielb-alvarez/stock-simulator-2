0e47e6ecf4f6cec8d8b355c6c9612cbad418ebff
eprdelapenapluto <eprdelapena@pluto-game.com>
2026-02-03 12:38:11 +0800

    Add query to /api/candles/:symbol main API

--- stock-simulator-v3/src/services/dev-coin-user/main/services.main.candles.ts
+++ stock-simulator-v3/src/services/dev-coin-user/main/services.main.candles.ts
@@ -4,6 +4,8 @@ import { TParamsTradingCandles, TResponseCandles, TDataTradingCandles } from "@/
 
 type CandleResponse<T = undefined> = Promise<AxiosResponse<TResponseCandles<T>>>
 
+const MAIN_API_URL_TEMPLATE = "/api/candles"
+
 class MainCandleService extends MainService {
 
     constructor() {
@@ -16,8 +18,15 @@ class MainCandleService extends MainService {
         data: TParamsTradingCandles
     }): CandleResponse<TDataTradingCandles[]> {
         try {
-            const endpoint: string = `/api/candles/${data?.symbol}`
-            return await this.client.get(endpoint);
+            const query = new URLSearchParams();
+            if (data?.limit) query.append("limit", data?.limit?.toString());
+            if (data?.interval) query.append("interval", data?.interval);
+            if (data?.exchange) query.append("exchange", data?.exchange);
+            if (data?.startTime) query.append("startTime", data?.startTime?.toString());
+            if (data?.endTime) query.append("endTime", data?.endTime?.toString());
+
+            const endpoint: string = `/${data?.symbol}?${query}`
+            return await this.client.get(MAIN_API_URL_TEMPLATE + endpoint);
         }
         catch (error) {
             console.error(error);
